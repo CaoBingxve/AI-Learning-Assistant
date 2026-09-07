0d0b108ab431e7a5c6ef10ce5b2a01c250b45132
caobingxue <2020593771@qq.com>
2026-09-07 17:22:50 +0800

    修改一下main

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { useUserStore } from './user.js'
+import { useUserStore } from './stores/user'
 
 
 async function bootstrap() {
